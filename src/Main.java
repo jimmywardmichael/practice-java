9c549af33c162af038341b9766441d04f29b80a4
James Ward <jimmywardmichael@gmail.com>
2023-01-09 13:04:55 -0600

    changes

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public  class Main{
+
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+        String myString = "40";
+        System.out.println(myString);
+//        String myString1 = '40'; cannot assign character single quotes to a string, string must be in ""
+//        String myString2 = 3.14159; cannot use a number for a string unless its wrapped in double quotes ""
+
+
+//        long myNumber; assigning long to a variable with no number results an error need an assigned value
+//        System.out.println(myNumber);
+//        long myNumber = (long) 3.14; have to cast long into parantheses to use a decimal.
+//        System.out.println(myNumber);
+
+
+    }
+}
